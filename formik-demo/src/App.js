5ba697d909eaf1c5a79a76195ca9b7c59ce3589b
intisatya123 <intisatya1762@gmail.com>
2025-03-03 17:44:18 +0530

    class

---
+++ formik-demo/src/App.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import logo from './logo.svg';
+import './App.css';
+import First from './components/First/first';
+
+function App() {
+  return (
+    <div className="App">
+      <First></First>
+    </div>
+  );
+}
+
+export default App;
